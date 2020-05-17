49104260dd7ae3fada4f88690754ccc46bde7ef6
Thomas Heller <thomas.m.heller@gmail.com>
2020-05-17 12:43:04 +0200

    disable attachbottom

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ git checkout master
 git checkout -b tmp
 git merge --no-edit config
 git merge --no-edit build
-patch <dwm-attachbottom-6.2.diff
+# patch <dwm-attachbottom-6.2.diff
 patch <dwm-warp-6.2.diff
 # patch <dwm-zoomswap-6.2.diff
 patch <dwm-remove-iscol-fix.diff
