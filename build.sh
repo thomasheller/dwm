1567dffabba1e65c76e237ee5f264245fd7d8bb5
Thomas Heller <thomas.m.heller@gmail.com>
2020-05-17 12:39:36 +0200

    disable zoomswap

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ git merge --no-edit config
 git merge --no-edit build
 patch <dwm-attachbottom-6.2.diff
 patch <dwm-warp-6.2.diff
-patch <dwm-zoomswap-6.2.diff
+# patch <dwm-zoomswap-6.2.diff
 patch <dwm-remove-iscol-fix.diff
 make
 git checkout build
