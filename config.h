a15bbc367dd849ce3467fce4dbcb60406828d205
Thomas Heller <thomas.m.heller@gmail.com>
2020-05-18 02:59:13 +0200

    add movestack patch

--- config.h
+++ config.h
@@ -69,6 +69,7 @@ static const char *browsercmd[]    = { "run_alternatives.sh", "chromium", "chrom
 static const char *passwordcmd[]   = { "keepass2", NULL };
 static const char *hibernatecmd[]  = { "sudo", "systemctl", "hibernate" };
 
+#include "movestack.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
@@ -83,6 +84,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
