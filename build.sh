ada12aeb64cda91d91d441d5db3bf391c33e5ed5
Thomas Heller <thomas.m.heller@gmail.com>
2020-06-13 10:26:19 +0200

    add resartsig patch

--- build.sh
+++ build.sh
@@ -13,6 +13,7 @@ patch <dwm-warp-6.2.diff
 patch <dwm-remove-iscol-fix.diff
 patch <dwm-movestack-6.1.diff
 patch <dwm-swallow-20200522-7accbcf.diff
+patch <dwm-restartsig-20180523-6.2.diff
 make
 git checkout build
 git branch -D tmp
