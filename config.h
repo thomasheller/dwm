1f1df35edac9e137241967e600133522f8e78677
Thomas Heller <thomas.m.heller@gmail.com>
2020-06-15 14:52:17 +0200

    st

--- config.h
+++ config.h
@@ -30,6 +30,7 @@ static const Rule rules[] = {
 	 */
 	/* class       instance    title       tags mask           isfloating   isterminal   noswallow   monitor */
 	{ "Alacritty", NULL,       NULL,             0,            0,           1,           0,          -1 },
+	{ "St",        NULL,       NULL,             0,            0,           1,           0,          -1 },
 	{ "Gimp",      NULL,       NULL,             1 << (8-1),   1,           0,           0,          -1 },
 	{ "Signal",    NULL,       NULL,             1 << (5-1),   0,           0,           0,          -1 },
 	{ "Shutter",   NULL,       NULL,             1 << (9-1),   0,           0,           0,          -1 },
@@ -64,7 +65,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[]      = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]       = { "alacritty", NULL };
+static const char *termcmd[]       = { "st", NULL };
+// static const char *termcmd[]       = { "alacritty", NULL };
 // static const char *termcmd[]       = { "alacritty", "-e", "tmux" };
 static const char *emojicmd[]      = { "emoji_menu.sh", NULL };
 static const char *screenshotcmd[] = { "screenshot.sh", NULL };
