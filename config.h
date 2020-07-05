9ecddfdf5d7c00f830e6945220ef19dc4cf5e1c5
Thomas Heller <thomas.m.heller@gmail.com>
2020-07-05 07:08:51 +0200

    adjust tags

--- config.h
+++ config.h
@@ -31,16 +31,18 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class       instance    title       tags mask           isfloating   isterminal   noswallow   monitor */
-	{ "Alacritty", NULL,       NULL,             0,            0,           1,           0,          -1 },
-	{ "st",        NULL,       NULL,             0,            0,           1,           0,          -1 },
-	{ "Gimp",      NULL,       NULL,             1 << (7-1),   1,           0,           0,          -1 },
-	{ "Telegram",  NULL,       NULL,             1 << (4-1),   0,           0,           0,          -1 },
-	{ "Signal",    NULL,       NULL,             1 << (5-1),   0,           0,           0,          -1 },
-	{ "Shutter",   NULL,       NULL,             1 << (8-1),   0,           0,           0,          -1 },
-	// { "Firefox",   NULL,       NULL,             1 << 8,       0,           0,           0,          -1 },
-	{ "KeePass",   NULL,       NULL,             NULL,         1,           0,           0,          -1 },
-	{ NULL,        NULL,       "Event Tester",   0,            0,           0,           1,          -1 }, // xev
+	/* class         instance  title            tags mask    isfloating  isterminal  noswallow  monitor */
+	{ "st",          NULL,     NULL,            0,           0,          1,          0,         -1 },
+	{ "Alacritty",   NULL,     NULL,            0,           0,          1,          0,         -1 },
+	{ "Chromium",    NULL,     NULL,            1 << (2-1),  0,          0,          0,         -1 },
+	{ "libreoffice", NULL,     NULL,            1 << (3-1),  0,          0,          0,         -1 },
+	{ "Telegram",    NULL,     NULL,            1 << (4-1),  0,          0,          0,         -1 },
+	{ "Signal",      NULL,     NULL,            1 << (5-1),  0,          0,          0,         -1 },
+	// 6
+	{ "Gimp",        NULL,     NULL,            1 << (7-1),  0,          0,          0,         -1 },
+	{ "Shutter",     NULL,     NULL,            1 << (8-1),  0,          0,          0,         -1 },
+	{ "KeePass",     NULL,     NULL,            0,           1,          0,          0,         -1 },
+	{ NULL,          NULL,     "Event Tester",  0,           0,          0,          1,         -1 }, // xev
 };
 
 /* layout(s) */
